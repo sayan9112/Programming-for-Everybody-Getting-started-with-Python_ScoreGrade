cecba384d5af9acc659bc26f88332ca6b2d5b6bf
Sayan Mukherjee <70065597+sayan9112@users.noreply.github.com>
2020-09-21 18:39:45 +0600

    Add files via upload

---
+++ ScoreGrade.py
@@ -0,0 +1,17 @@
+score = input(score = input("Enter Score:" )
+s = float(score)
+x = &#39;Error&#39;
+if s &gt;= 0.9:
+x = &#39;A&#39;
+elif s &gt;=0.8:
+x=&#39;B&#39;
+elif s &gt;=0.7:
+x=&#39;C&#39;
+elif s &gt;= 0.6:
+x=&#39;D&#39;
+elif s &lt; .6:
+x =&#39;F&#39;
+else:
+x =&quot;Out of Range&quot;
+print (x)
+
